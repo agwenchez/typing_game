6394bfa56b0385f8c60851bbdf5637989656f3d4
agwenchez <agweraenock76@gmail.com>
2020-09-22 03:52:23 -0400

    some minor bug fixes

--- script.js
+++ script.js
@@ -4,13 +4,28 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZeros (time){
+    if(time <=9){
+        time = "0" + time;
+    }
 
+    return time
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
-    
+    let currentTime = leadingZeros(timer[0]) + ":" + leadingZeros(timer[1]) + ":"+ leadingZeros(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60)
+    timer[1] = Math.floor((timer[3]/100)- timer[0]*60)
+    timer[2] = Math.floor(timer[3] - (timer[1]*100) -(timer[0]*6000) )
+
+
 }
 
 
